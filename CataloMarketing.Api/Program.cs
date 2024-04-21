84100b84b0fc069072983b2793e188cefda9b501
denisiorodrigues <denisio@ymail.com>
2024-04-21 19:34:29 -0300

    feat: connection string configuration

--- CataloMarketing.Api/Program.cs
+++ CataloMarketing.Api/Program.cs
@@ -1,9 +1,17 @@
+using CataloMarketing.Api;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string mySqlConnection = builder.Configuration.GetConnectionString("default");
+builder.Services.AddDbContext<ApiContext>(options => 
+    options.UseMySql(mySqlConnection, 
+    ServerVersion.AutoDetect(mySqlConnection)));
 
 var app = builder.Build();
 

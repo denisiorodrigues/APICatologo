b8a7dfd63bb6e873bb5ad49e1a8e9652a1dd7337
denisiorodrigues <denisio@ymail.com>
2024-04-21 15:11:56 -0300

    feat: add api context

---
+++ CataloMarketing.Api/Context/ApiContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CataloMarketing.Api;
+
+public class ApiContext : DbContext
+{
+  public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+  {
+  }
+  
+  public DbSet<Category> Categories { get; set; }
+  
+  public DbSet<Product> Products { get; set; }
+}

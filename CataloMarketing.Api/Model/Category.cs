c3b87f4ed68d66af05363e6fada3064d52c1d688
denisiorodrigues <denisio@ymail.com>
2024-04-21 14:50:59 -0300

    feat(model): Create product and update Category

--- CataloMarketing.Api/Model/Category.cs
+++ CataloMarketing.Api/Model/Category.cs
@@ -4,4 +4,7 @@ public class Category
 {
   public int Id { get; set; }
 
+  public string? Name { get; set; }
+
+  public string? ImageUri { get; set; }
 }
---
+++ CataloMarketing.Api/Model/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace CataloMarketing.Api;
+
+public class Product
+{
+  public int Id { get; set; }
+  public string? Name { get; set; }
+  public string? Description { get; set; }
+  public Decimal Price { get; set; }   
+  public string? ImageUri { get; set; }
+  public float Stok { get; set; }
+  public DateTime CreatedDate { get; set; }
+  public int CategoryId { get; set; }
+  public Category? Category { get; set; }
+}

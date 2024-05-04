404a7aac9a84b1a404ce35d23c25ff241209a5df
denisiorodrigues <denisio@ymail.com>
2024-05-04 14:34:56 -0300

    feat: insert categories

---
+++ CataloMarketing.Api/Migrations/20240504173041_InsertCategories.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CataloMarketing.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into Categories (Name, ImageUri) values ('Bebidas', 'bebidas.jpg')");
+            migrationBuilder.Sql("Insert into Categories (Name, ImageUri) values ('Lanches', 'lanches.jpg')");
+            migrationBuilder.Sql("Insert into Categories (Name, ImageUri) values ('Sobremeses', 'Sobremeses.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("delete from Categories");
+        }
+    }
+}
